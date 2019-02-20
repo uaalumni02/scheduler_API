c486a11b953b7790d5045f0f690a8ad60d8507ab
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-02-19 19:49:20 -0500

    updated model

--- models/service.js
+++ models/service.js
@@ -1,12 +1,34 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+const isValidService = (service) => {
+   const regExp = /^[a-z]{2,}$/i;
+   return regExp.test(service)
+ };
+
+
+const isValidPrice = (price) => {
+   const regExp = /[0-9]?[0-9]?(\.[0-9][0-9]?)?/i
+   return regExp.test(price)
+ };
+
 const serviceInformationSchema = mongoose.Schema({
 
    _id: mongoose.Schema.Types.ObjectId,
    Services: {
       type: String,
-   }
+      required: [true, 'Service is required'],
+      validate: [isValidService, 'Please enter a valid Service'],
+   },
+   appointmentDate: {
+      type: Number,
+      required: true 
+  },
+   Price: {
+      type: Number,
+      required: [true, 'Price is required'],
+      validate: [isValidPrice, 'Please enter a valid Price'],
+   },
 });
 
 
