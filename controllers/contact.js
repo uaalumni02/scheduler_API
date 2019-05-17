09a83b871d4bbe3ca0963064576d5edbf6813c4f
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-05-16 21:57:00 -0400

    added email functionality

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,17 +1,34 @@
 import * as db from '../db/db';
 import Contact from '../models/contact';
+import gmail from 'node-gmailer';
+
+const TWILIO_ID = process.env.TWILIO_ACCOUNT_ID;
 
 class Message {
     static async addContactMessage(req, res) {
-       const newMessageData = {
-           firstName: req.body.firstName,
-           lastName: req.body.lastName,
-           email: req.body.email,
-           phone: req.body.phone,
-           message: req.body.message
-       }
+        const newMessageData = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+            message: req.body.message
+        }
+        const customerMessage = 'You have a message from ' + newMessageData.firstName  + ' ' + newMessageData.lastName  + ' that says: ' + newMessageData.message;
+        const recipient = process.env.GMAIL_ADDRESS;
+        const messageData = {
+            subject: 'Customer Message',
+            text: customerMessage,
+        }
+        const sendHandler = () => {
+            gmail.send(recipient, messageData)
+                .then(response => {
+                })
+                .catch(error => {
+                });
+        }
         try {
             const addMessage = await db.addNewMessage(Contact, newMessageData)
+            sendHandler()
             return res.status(200).json(addMessage)
         } catch (error) {
             res.status(500).json({ error: error })
@@ -29,3 +46,4 @@ class Message {
 
 
 export default Message;
+
